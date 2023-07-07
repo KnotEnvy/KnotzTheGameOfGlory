4090aa71c2988a4d5d8bf26d284f3bddbf826461
KnotEnvy <jwsnyder@gmail.com>
2023-07-06 21:53:40 -0400

    Changed the width, height, and maxFrame properties of the WormEnemy class.n

--- enemy.js
+++ enemy.js
@@ -68,14 +68,14 @@ export class WormEnemy extends Enemy {
     constructor(game){
         super();
         this.game = game;
-        this.width = 60;
-        this.height = 87;
+        this.width = 80.33;
+        this.height = 60;
         this.x = this.game.width;
         this.y = this.game.height - this.height - this.game.groundMargin;
         this.image = enemy_worm; // replace with your worm image
         this.speedX = Math.random() + 2; // faster than the fly enemy
         this.speedY = 0;
-        this.maxFrame = 1;
+        this.maxFrame = 5;
     }
     stopSound() {
         super.stopSound();
