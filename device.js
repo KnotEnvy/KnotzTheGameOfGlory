4825b33aeb62a754f79fe20226b9e62845dd232e
KnotEnvy <jwsnyder@gmail.com>
2023-07-07 09:27:31 -0400

    Removed console log statement in isMobileDevice function.

--- device.js
+++ device.js
@@ -1,5 +1,5 @@
 export function isMobileDevice() {
     const isMobile = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    console.log(`Is mobile: ${isMobile}`);
+    // console.log(`Is mobile: ${isMobile}`);
     return isMobile;
 }
